929a84890db7920bc7a2295fca06e5d6c8b0c3b0
ElmanDev <xzample101@gmail.com>
2024-02-05 12:36:32 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,166 @@
+<script setup lang="ts">
+import TableLite from 'vue3-table-lite'
+import { computed, onMounted, reactive } from 'vue'
+import axios from 'axios'
+import { paymentsBybit } from '@/constants/payments'
+
+const table = reactive({
+  isLoading: false,
+  columns: [
+    {
+      label: 'Цена',
+      field: 'price',
+      sortable: true,
+      width: '20%',
+      isKey: true
+    },
+    {
+      label: 'Маркет',
+      field: 'market',
+      sortable: true
+    },
+    {
+      label: 'Доступно',
+      field: 'lastQuantity',
+      sortable: true,
+      width: '15%'
+    },
+    {
+      label: 'Лимиты',
+      field: 'limits',
+      sortable: true,
+      width: '15%'
+    },
+    {
+      label: 'Способ оплаты',
+      field: 'payments',
+      sortable: true,
+      width: '20%'
+    },
+    {
+      label: 'Комментарий',
+      field: 'remark',
+      width: '100%'
+    }
+  ],
+  rows: [],
+  totalRecordCount: computed(() => table.rows.length),
+  sortable: {
+    order: 'price',
+    sort: 'desc'
+  }
+})
+
+const getPaymentName = (id: number) => paymentsBybit.find((payment) => payment.id === id)
+
+const fetchBybitSell = async () => {
+  try {
+    const { data } = await axios.get('http://localhost:3000/bybit-sell')
+
+    table.rows.push(
+      ...data.result.items.map((item) => {
+        return {
+          price: item.price,
+          market: 'Bybit',
+          lastQuantity: item.lastQuantity,
+          limits: `${item.minAmount} - ${item.maxAmount}`,
+          payments: item.payments.map((id: string) => getPaymentName(+id)?.name || id).join(', '),
+          remark: item.remark
+        }
+      })
+    )
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const fetchHTXSell = async () => {
+  try {
+    const { data } = await axios.get('http://localhost:3000/htx-sell')
+
+    table.rows.push(
+      ...data.data.map((item) => {
+        return {
+          price: item.price,
+          market: 'HTX',
+          lastQuantity: item.tradeCount,
+          limits: `${item.minTradeLimit} - ${item.maxTradeLimit}`,
+          payments: item.payMethods.map((payMethod) => payMethod.name).join(', '),
+          remark: ''
+        }
+      })
+    )
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const fetchAbcexSell = async () => {
+  try {
+    const { data } = await axios.get('http://localhost:3000/abcex-sell')
+
+    table.rows.push(
+      ...data.map((item) => {
+        return {
+          price: item.price,
+          market: 'Abcex',
+          lastQuantity: item.volume,
+          limits: '',
+          payments: 'Cash',
+          remark: ''
+        }
+      })
+    )
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const fetchCommexSell = async () => {
+  try {
+    const { data } = await axios.get('http://localhost:3000/commex-sell')
+
+    table.rows.push(
+      ...data.data.map((item) => {
+        return {
+          price: item.adDetailResp.price,
+          market: 'Commex',
+          lastQuantity: item.adDetailResp.tradableQuantity,
+          limits: `${item.adDetailResp.minSingleTransAmount} - ${item.adDetailResp.maxSingleTransAmount}`,
+          payments: item.adDetailResp.tradeMethods
+            .map((method) => method.tradeMethodName)
+            .join(', '),
+          remark: ''
+        }
+      })
+    )
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+onMounted(async () => {
+  table.isLoading = true
+  await Promise.all([fetchBybitSell(), fetchHTXSell(), fetchAbcexSell(), fetchCommexSell()])
+  table.isLoading = false
+})
+</script>
+
+<template>
+  <table-lite
+    :is-loading="table.isLoading"
+    :columns="table.columns"
+    :rows="table.rows"
+    :total="table.totalRecordCount"
+    :sortable="table.sortable"
+    :is-static-mode="true"
+    :page-size="table.totalRecordCount"
+    class="table"
+  />
+</template>
+
+<style scoped>
+.table {
+  width: 100%;
+}
+</style>
---
+++ src/constants/payments.ts
@@ -0,0 +1,63 @@
+interface IPayment {
+  id: number
+  name: string
+}
+
+export const paymentsBybit: IPayment[] = [
+  {
+    id: 62,
+    name: 'QIWI'
+  },
+  {
+    id: 63,
+    name: 'Raiffeisen Bank Aval'
+  },
+  {
+    id: 64,
+    name: 'Raiffeisenbank'
+  },
+  {
+    id: 581,
+    name: 'Tinkoff'
+  },
+  {
+    id: 582,
+    name: 'Rosbank'
+  },
+  {
+    id: 583,
+    name: 'Alfabank'
+  },
+  {
+    id: 584,
+    name: 'Local Bank(R-Green)'
+  },
+  {
+    id: 585,
+    name: 'Sberbank'
+  },
+  {
+    id: 383,
+    name: 'MIR'
+  },
+  {
+    id: 533,
+    name: 'Russian Standard Bank'
+  },
+  {
+    id: 51,
+    name: 'Payeer'
+  },
+  {
+    id: 14,
+    name: 'Bank Transfer'
+  },
+  {
+    id: 382,
+    name: 'SBP'
+  },
+  {
+    id: 1,
+    name: 'A-Bank'
+  }
+]
